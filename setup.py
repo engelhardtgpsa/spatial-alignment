df2726a206122292d23bde1758643ceaca1cb5ca
emilyrainschang <emilyrainschang@gmail.com>
2025-06-03 10:08:46 -0700

    Loosen GPSA pandas requirement to allow 2.x

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     install_requires=[
         "torch==2.2.2",
         "numpy>=1.21,<1.25",
-        "pandas==1.3.5",
+        "pandas>=1.3.5,<2.3",
         "scikit-learn==1.0.2",
         "pcpca==0.3",
         "plottify==1.1",
